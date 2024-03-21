79ba0acfdbc1913a44d21364213af80f06c1cc19
Faruk Derdiyok <farukderdiyok01@gmail.com>
2024-03-21 11:05:40 +0300

    recipe dataseti eklendi

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -13,64 +13,151 @@ ALTER TABLE if EXISTS c_recipeingredient ADD CONSTRAINT FK1y2it0upytd6c59fave8aj
 ALTER TABLE if EXISTS c_recipeingredient ADD CONSTRAINT FKbmrm3umi1x7oucc519kxmjt9g FOREIGN KEY (recipe_id) REFERENCES recipe;
 
 -- insert ingredients
-INSERT INTO ingredient (name) VALUES ('Onion');
-INSERT INTO ingredient (name) VALUES ('Patato');
-INSERT INTO ingredient (name) VALUES ('Apple');
-INSERT INTO ingredient (name) VALUES ('Cucumber');
-INSERT INTO ingredient (name) VALUES ('Egg');
-INSERT INTO ingredient (name) VALUES ('Tomato Paste');
-INSERT INTO ingredient (name) VALUES ('Milk');
-INSERT INTO ingredient (name) VALUES ('Garlic');
-INSERT INTO ingredient (name) VALUES ('Spinach');
-INSERT INTO ingredient (name) VALUES ('Cheese');
-INSERT INTO ingredient (name) VALUES ('Rice');
-INSERT INTO ingredient (name) VALUES ('Banana');
-INSERT INTO ingredient (name) VALUES ('Parsley');
-INSERT INTO ingredient (name) VALUES ('Carrot');
-INSERT INTO ingredient (name) VALUES ('Broccoli');
-INSERT INTO ingredient (name) VALUES ('Butter');
-INSERT INTO ingredient (name) VALUES ('Yogurt');
-INSERT INTO ingredient (name) VALUES ('Lemon');
-INSERT INTO ingredient (name) VALUES ('Strawberry');
-INSERT INTO ingredient (name) VALUES ('Zucchini');
-INSERT INTO ingredient (name) VALUES ('Aubergine');
-INSERT INTO ingredient (name) VALUES ('Pepper');
-INSERT INTO ingredient (name) VALUES ('Flour');
-INSERT INTO ingredient (name) VALUES ('Olive');
-INSERT INTO ingredient (name) VALUES ('Sausage');
-INSERT INTO ingredient (name) VALUES ('Biscuit');
-INSERT INTO ingredient (name) VALUES ('Chicken');
-INSERT INTO ingredient (name) VALUES ('Avocado');
-INSERT INTO ingredient (name) VALUES ('Orange');
-INSERT INTO ingredient (name) VALUES ('Chocolate');
-INSERT INTO ingredient (name) VALUES ('Mushroom');
-INSERT INTO ingredient (name) VALUES ('Baking Powder');
-INSERT INTO ingredient (name) VALUES ('Walnut');
-INSERT INTO ingredient (name) VALUES ('Hazelnut');
-INSERT INTO ingredient (name) VALUES ('Corn');
-INSERT INTO ingredient (name) VALUES ('Tuna');
-INSERT INTO ingredient (name) VALUES ('Lettuce');
-INSERT INTO ingredient (name) VALUES ('Cauliflower');
-INSERT INTO ingredient (name) VALUES ('Pasta');
-INSERT INTO ingredient (name) VALUES ('Cream');
-INSERT INTO ingredient (name) VALUES ('Salami');
-INSERT INTO ingredient (name) VALUES ('Sausage');
-INSERT INTO ingredient (name) VALUES ('Peach');
-INSERT INTO ingredient (name) VALUES ('Leek');
-INSERT INTO ingredient (name) VALUES ('Celery');
-INSERT INTO ingredient (name) VALUES ('Peanut');
-INSERT INTO ingredient (name) VALUES ('Cherry');
-INSERT INTO ingredient (name) VALUES ('Quince');
-INSERT INTO ingredient (name) VALUES ('Pomegranate');
-INSERT INTO ingredient (name) VALUES ('Pineapple');
-INSERT INTO ingredient (name) VALUES ('Coffee');
-INSERT INTO ingredient (name) VALUES ('Spring Onion');
-INSERT INTO ingredient (name) VALUES ('Peas');
+INSERT INTO ingredient (id, name) VALUES (1, 'Soğan');
+INSERT INTO ingredient (id, name) VALUES (2, 'Patates');
+INSERT INTO ingredient (id, name) VALUES (3, 'Elma');
+INSERT INTO ingredient (id, name) VALUES (4, 'Domates');
+INSERT INTO ingredient (id, name) VALUES (5, 'Salatalık');
+INSERT INTO ingredient (id, name) VALUES (6, 'Yumurta');
+INSERT INTO ingredient (id, name) VALUES (7, 'Sarımsak');
+INSERT INTO ingredient (id, name) VALUES (8, 'Ispanak');
+INSERT INTO ingredient (id, name) VALUES (9, 'Peynir');
+INSERT INTO ingredient (id, name) VALUES (10, 'Pirinç');
+INSERT INTO ingredient (id, name) VALUES (11, 'Muz');
+INSERT INTO ingredient (id, name) VALUES (12, 'Maydanoz');
+INSERT INTO ingredient (id, name) VALUES (13, 'Havuç');
+INSERT INTO ingredient (id, name) VALUES (14, 'Brokoli');
+INSERT INTO ingredient (id, name) VALUES (15, 'Limon');
+INSERT INTO ingredient (id, name) VALUES (16, 'Çilek');
+INSERT INTO ingredient (id, name) VALUES (17, 'Kabak');
+INSERT INTO ingredient (id, name) VALUES (18, 'Patlıcan');
+INSERT INTO ingredient (id, name) VALUES (19, 'Biber');
+INSERT INTO ingredient (id, name) VALUES (20, 'Zeytin');
+INSERT INTO ingredient (id, name) VALUES (21, 'Sucuk');
+INSERT INTO ingredient (id, name) VALUES (22, 'Avokado');
+INSERT INTO ingredient (id, name) VALUES (23, 'Portakal');
+INSERT INTO ingredient (id, name) VALUES (24, 'Mantar');
+INSERT INTO ingredient (id, name) VALUES (25, 'Ceviz');
+INSERT INTO ingredient (id, name) VALUES (26, 'Fındık');
+INSERT INTO ingredient (id, name) VALUES (27, 'Marul');
+INSERT INTO ingredient (id, name) VALUES (28, 'Karnabahar');
+INSERT INTO ingredient (id, name) VALUES (29, 'Pırasa');
+INSERT INTO ingredient (id, name) VALUES (30, 'Kereviz');
+INSERT INTO ingredient (id, name) VALUES (31, 'Vişne');
+INSERT INTO ingredient (id, name) VALUES (32, 'Ayva');
+INSERT INTO ingredient (id, name) VALUES (33, 'Nar');
+INSERT INTO ingredient (id, name) VALUES (34, 'Ananas');
+INSERT INTO ingredient (id, name) VALUES (35, 'Bezelye');
+INSERT INTO ingredient (id, name) VALUES (36, 'Tavuk');
+INSERT INTO ingredient (id, name) VALUES (37, 'Et');
+INSERT INTO ingredient (id, name) VALUES (38, 'Kıyma');
 
--- insert recipes
-INSERT INTO recipe (calories,description,image,name) VALUES (300, 'Mükemmel Menemenin Sırı Burada', 'https://img.freepik.com/free-photo/fried-tomatoes-with-eggs-table_140725-4724.jpg?t=st=1709576385~exp=1709579985~hmac=00c392a8a8dcb6ee995d17ec7e96b3d6b96d6d78c89b619481186081682ca923&w=1380', 'Menemen');
 
--- insert recipe - ingredient
-INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (1,1);
-INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (1,4);
+-- menemen
+INSERT INTO recipe (id, name, calories, description, image) VALUES (1, 'Sebzeli Omlet', 188,
+                                                                'Taze sebzelerle dolu lezzetli ve besleyici bir omlet.',
+                                                                'https://i2.nefisyemektarifleri.com/user-photos/2021/01/24/nyt-up-2480400_recipe-2021-01-24-13-36-01.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (1,1); --soğan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (1,4); --domates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (1,6); --yumurta
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (1,19); --biber
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (1,24); --mantar
 
+
+-- akdeniz salatası
+INSERT INTO recipe (id, name, calories, description, image) VALUES (2, 'Akdeniz Salatası', 120,
+                                                                    'Akdeniz lezzetlerinden ilham alan ferahlatıcı bir salata.',
+                                                                    'https://www.greensalads.com.tr/Upload/urunler/11420232343693akdenizl-salata.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (2, 4); --domates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (2, 5); --salatalık
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (2, 9); --peynir
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (2, 15); --limon
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (2, 19); --biber
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (2, 20); --zeytin
+
+-- tavuk sote
+INSERT INTO recipe (id, name, calories, description, image) VALUES (3, 'Tavuk Sote', 169,
+                                                                    'Çeşitli sebzelerle hızlı ve lezzetli bir tavuk sote.',
+                                                                    'https://i.ytimg.com/vi/SOlm4nkd-J4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDN4cC5RLM7CSuGSwyMTAumhBvqAA');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (3, 1); --soğan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (3, 7); --sarımsak
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (3, 13); --havuç
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (3, 14); --brokoli
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (3, 19); --biber
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (3, 36); --tavuk
+
+
+-- meyve smoothie
+INSERT INTO recipe (id, name, calories, description, image) VALUES (4, 'Meyve Smoothie', 229,
+                                                                    'Taze meyvelerle yapılan serinletici ve besleyici bir smoothie.',
+                                                                    'https://www.tarifinisevdim.com/uploads/others/cc15e350-0b93-4fb0-82fe-a465bb86792a.Jpeg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (4, 11); --muz
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (4, 16); --çilek
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (4, 23); --portakal
+
+
+-- yeşil salata
+INSERT INTO recipe (id, name, calories, description, image) VALUES (5, 'Yeşil Salata', 120,
+                                                                    'Taze yeşil yapraklarla yapılan hafif ve sağlıklı bir salata.',
+                                                                    'https://www.diyetkolik.com/site_media/media/foodrecipe_images/yesil-salata.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (5, 12); --marul
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (5, 15); --maydanoz
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (5, 27); --limon
+
+
+-- tavuk salatası
+INSERT INTO recipe (id, name, calories, description, image) VALUES (6, 'Tavuk Salata', 120,
+                                                                    'Taze yeşil yapraklarla yapılan hafif ve sağlıklı bir salata.',
+                                                                    'https://vicivedi.com/img/691957.webp');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (6, 4); --domates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (6, 27); --marul
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (6, 36); --tavuk
+
+
+-- et salatası
+INSERT INTO recipe (id, name, calories, description, image) VALUES (7, 'Et Salatası', 120,
+                                                                    'Dana eti ve sebzelerle hazırlanan doyurucu bir salata.',
+                                                                    'https://market.amasyaeturunleri.com.tr/wp-content/uploads/evde-lezzetler-hardal-soslu-bonfileli-salata.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (7, 1); --soğan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (7, 4); --domates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (7, 27); --marul
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (7, 37); --et
+
+
+-- sebze kızartması
+INSERT INTO recipe (id, name, calories, description, image) VALUES (8, 'Sebze Kızartma', 120,
+                                                                    'Farklı sebzelerin kızartılarak hazırlandığı lezzetli bir sebze yemeği.',
+                                                                    'https://cdn.yemek.com/mnresize/1250/833/uploads/2015/08/karisik-kizartma-yenionecikan.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (8, 2); --patates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (8, 7); --sarımsak
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (8, 13); --havuç
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (8, 17); --patlıcan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (8, 18); --kabak
+
+
+-- Zeytinyağlı Kabak Yemeği
+INSERT INTO recipe (id, name, calories, description, image) VALUES (9, 'Zeytinyağlı Kabak Yemeği', 120,
+                                                                    'Zeytinyağında pişirilmiş, baharatlarla lezzetlendirilmiş sağlıklı bir kabak yemeği.',
+                                                                    'https://cdn.yemek.com/mnresize/1250/833/uploads/2019/07/kabak-yemegi-onecikan.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (9, 1); --soğan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (9, 4); --domates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (9, 7); --sarımsak
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (9, 17); --kabak
+
+
+-- Zeytinyağlı Patlıcan Yemeği
+INSERT INTO recipe (id, name, calories, description, image) VALUES (10, 'Zeytinyağlı Patlıcan Yemeği', 120,
+                                                                    'Zeytinyağında pişirilmiş, baharatlarla lezzetlendirilmiş sağlıklı bir patlıcan yemeği.',
+                                                                    'https://i20.haber7.net/resize/1280x720//haber/haber7/photos/2023/14/zeytinyagli_patlican_yemegi_nasil_yapilir_zeytinyagli_patlican_yemegi_tarifi_1680596072_057.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (10, 1); --soğan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (10, 4); --domates
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (10, 18); --sarımsak
+
+
+-- Zeytinyağlı Brokoli Yemeği
+INSERT INTO recipe (id, name, calories, description, image) VALUES (11, 'Zeytinyağlı Brokoli Yemeği', 120,
+                                                                    'Sağlıklı ve lezzetli bir brokoli yemeği zeytinyağı ve baharatlarla hazırlanmıştır.',
+                                                                    'https://cdn.yemek.com/mnresize/1250/833/uploads/2022/01/zeytinyagli-brokoli-yemegi-one-cikan.jpg');
+INSERT INTO c_recipeingredient (recipe_id,ingredient_id) VALUES (11, 1); --soğan
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (11, 7); --sarımsak
+INSERT INTO c_recipeingredient (recipe_id, ingredient_id) VALUES (11, 14); --brokoli
